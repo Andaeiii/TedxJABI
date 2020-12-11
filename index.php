a6213b00823fc24525794a1a4ff903d2d76a06f4
Ande Caleb <andaeiii@aol.com>
2020-12-11 21:49:47 +0100

    Website Commit

---
+++ index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>TinkerBiz Services - Official Website</title>
+
+  <!-- Font Awesome Icons -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+
+
+  <!-- Plugin CSS -->
+  <link href="/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Theme CSS - Includes Bootstrap -->
+  <link href="/css/creative.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">TinkerBiz Services</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Masthead -->
+  <header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end">
+          <h1 class="text-uppercase text-white font-weight-bold">An Agro-business Company in Nigeria </h1>
+          <hr class="divider my-4">
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-white-75 font-weight-light mb-5">We Consult, import, export, negotiate, grow, Source, process, Manufacture, transport, buy and sell, All things Agricultural </p>
+          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+
+ <section class="page-section about bg-primary" id="about">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="text-white mt-0">Get To Know More About Us!</h2>
+          <hr class="divider light my-4">
+          <p class="text-white-50 mb-4">Tinker Business Services with Rc: 847616 was incorporated in 2009, with her headquarters located at 41, old Oturkpo Road, High Level, Makurdi, Benue State, Nigeria.  Tinker Business Services Limited is a diversified firm dedicated to provide services ranging from Agriculture, Human Capital Development and trading worldwide.<br/><br/>
+We are an Agric-business Company Operating in Nigeria. We Consult, import, export, negotiate, grow, Source, process, Manufacture, transport, trade and market different agri-products , buy and sell Agricultural equipment as well as allied Machineries.<br/><br/>
+We also offer Consultancy services to cooperatives, organized bodies and act as Intermediaries to companies.
+At Tinker Business, our aim is to provide services that are both comprehensive and relevant to the needs of our clients.
+</p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- About Section -->
+  <section class="page-section bg-dark" id="vision">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+
+
+          <h2 class="text-white mt-0">Our Mission</h2>
+          <hr class="divider light my-4">
+          <p class="text-white-50 mb-4">Our “MISSION” is to provide effective and efficient services to our clients while keeping to the tenets of the profession and all relevant laws of the Federal  Republic of Nigeria and the World.</p>
+
+  <h2 class="text-white mt-0">Our Vision</h2>
+          <hr class="divider light my-4">
+          <p class="text-white-50 mb-4">Our “VISION” is to become a world leader in providing High tech Solutions to the world.</p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Services Section -->
+  <section class="page-section" id="services">
+    <div class="container">
+      <h2 class="text-center mt-0">Our Services</h2>
+      <hr class="divider my-4">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="/img/iconx.jpg" width="80"/>
+            <h3 class="h4 mb-2">Crop Production</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="/img/iconx.jpg" width="80"/>
+            <h3 class="h4 mb-2">Processing</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="/img/iconx.jpg" width="80"/>
+            <h3 class="h4 mb-2">Agro Produce Marketing</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="/img/iconx.jpg" width="80"/>
+            <h3 class="h4 mb-2">Contract Buying</h3>
+          </div>
+        </div>
+
+
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="/img/iconx.jpg" width="80"/>
+            <h3 class="h4 mb-2">Wholesaling</h3>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="/img/iconx.jpg" width="80"/>
+            <h3 class="h4 mb-2">Marketing of Agricultural Equipment</h3>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="/img/iconx.jpg" width="80"/>
+            <h3 class="h4 mb-2">Human Capital Development</h3>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 text-center">
+          <div class="mt-5">
+            <img src="/img/iconx.jpg" width="80"/>
+            <h3 class="h4 mb-2">Consultancy</h3>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+
+  <!-- Portfolio Section -->
+  <section id="portfolio">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Perinnial Crop Farming
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Rice Farming
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Modernized Farming
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/4.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Trading
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/5.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+                Hawking/Traders
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/6.jpg" alt="">
+            <div class="portfolio-box-caption p-3">
+              <div class="project-category text-white-50">
+                Distributing(large scale)
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Call to Action Section -->
+  <section class="page-section bg-dark profile text-white">
+    <div class="container text-center">
+      <h2 class="mb-4">Get A Copy of our Company Profile </h2>
+      <a class="btn btn-light btn-xl" href="/docs/company_profile.pdf">Download Now!</a>
+    </div>
+  </section>
+
+  <!-- Contact Section -->
+  <section class="page-section contact" id="contact">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-0">Let's Get In Touch!</h2>
+          <hr class="divider my-4">
+          <p class="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+          <div>(+234) 0703 156 4908</div>
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+          <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <a class="d-block" href="mailto:info@tinkerbizservices.org">info@tinkerbizservices.org</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="bg-light py-5">
+    <div class="container">
+      <div class="small text-center text-muted">Copyright &copy; <?php echo date('Y'); ?> - TinkerBizServices.org</div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/creative.min.js"></script>
+
+</body>
+
+</html>
